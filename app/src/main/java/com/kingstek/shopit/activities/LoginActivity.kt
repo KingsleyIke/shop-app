54747a8cdea0c96eb2c0e9e51d5f64f2662030c5
KingsleyIke <kingsdanike@gmail.com>
2022-01-03 11:31:56 +0100

    register activity

--- app/src/main/java/com/kingstek/shopit/activities/LoginActivity.kt
+++ app/src/main/java/com/kingstek/shopit/activities/LoginActivity.kt
@@ -1,11 +1,13 @@
 package com.kingstek.shopit.activities
 
+import android.content.Intent
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.WindowInsets
 import android.view.WindowManager
 import com.kingstek.shopit.R
+import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,6 +22,11 @@ class LoginActivity : AppCompatActivity() {
                 WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN,
             )
+        }
+
+        tv_register.setOnClickListener{
+            val intent = Intent(this@LoginActivity, RegisterActivity::class.java)
+            startActivity(intent)
         }
     }
 }
