2d2d189f49401f194e73566d938d53e00f0c3f7e
KingsleyIke <kingsdanike@gmail.com>
2022-01-10 21:36:34 +0100

    save user to firestore

--- app/src/main/java/com/kingstek/shopit/firestore/FirestoreClass.kt
+++ app/src/main/java/com/kingstek/shopit/firestore/FirestoreClass.kt
@@ -1,10 +1,26 @@
 package com.kingstek.shopit.firestore
 
+import android.util.Log
 import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.SetOptions
+import com.kingstek.shopit.activities.RegisterActivity
+import com.kingstek.shopit.models.User
 
 class FirestoreClass {
 
     private val mFirestore = FirebaseFirestore.getInstance()
 
+    fun registerUser (activity: RegisterActivity, userInfo: User) {
 
+        mFirestore.collection("users")
+            .document(userInfo.id)
+            .set(userInfo, SetOptions.merge())
+            .addOnSuccessListener {
+                activity.userRegistrationSuccess()
+            }
+            .addOnFailureListener{ e ->
+                activity.hideProgressDialog()
+                Log.e( activity.javaClass.simpleName, "Error while registering the user", e)
+            }
+    }
 }
---
+++ app/src/main/java/com/kingstek/shopit/models/User.kt
@@ -0,0 +1,12 @@
+package com.kingstek.shopit.models
+
+data class User (
+    val id: String = "",
+    val firstName: String = "",
+    val lastName: String = "",
+    val email: String = "",
+    val image: String = "",
+    val mobile: Long = 0,
+    val gender: String = "",
+    val profileCompleted: Int = 0
+)
