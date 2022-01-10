276883895b212d548a3c236932f15ba8a4cadf91
KingsleyIke <kingsdanike@gmail.com>
2022-01-10 20:50:47 +0100

    fire store dependecies

--- app/src/main/java/com/kingstek/shopit/firestore/FirestoreClass.kt
+++ app/src/main/java/com/kingstek/shopit/firestore/FirestoreClass.kt
@@ -1,4 +1,10 @@
 package com.kingstek.shopit.firestore
 
+import com.google.firebase.firestore.FirebaseFirestore
+
 class FirestoreClass {
+
+    private val mFirestore = FirebaseFirestore.getInstance()
+
+
 }
