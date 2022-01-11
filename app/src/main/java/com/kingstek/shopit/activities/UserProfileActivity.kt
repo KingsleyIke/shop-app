a6ab61ed6ae9a51528e0146cecd96ec9dcb492bd
KingsleyIke <kingsdanike@gmail.com>
2022-01-11 01:56:13 +0100

    select images

--- app/src/main/java/com/kingstek/shopit/activities/UserProfileActivity.kt
+++ app/src/main/java/com/kingstek/shopit/activities/UserProfileActivity.kt
@@ -1,9 +1,12 @@
 package com.kingstek.shopit.activities
 
 import android.Manifest
+import android.app.Activity
+import android.content.Intent
 import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.Toast
 import androidx.core.app.ActivityCompat
@@ -16,6 +19,7 @@ import kotlinx.android.synthetic.main.activity_register.et_email
 import kotlinx.android.synthetic.main.activity_register.et_first_name
 import kotlinx.android.synthetic.main.activity_register.et_last_name
 import kotlinx.android.synthetic.main.activity_user_profile.*
+import java.io.IOException
 
 class UserProfileActivity : BaseActivity(), View.OnClickListener {
 
@@ -51,7 +55,9 @@ class UserProfileActivity : BaseActivity(), View.OnClickListener {
                 R.id.iv_user_photo -> {
 
                     if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) {
-                        showErrorSnackBar("You ald=ready have the storage permision.", false)
+
+                        Constants.showImageChooser(this@UserProfileActivity)
+//                        showErrorSnackBar("You ald=ready have the storage permision.", false)
                     } else {
 
                         ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE), Constants.READ_STORAGE_PERMISSION_CODE)
@@ -72,11 +78,36 @@ class UserProfileActivity : BaseActivity(), View.OnClickListener {
         if (requestCode == Constants.READ_STORAGE_PERMISSION_CODE) {
             if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
 
-                showErrorSnackBar("The storage Permission is granted", false)
+//                showErrorSnackBar("The storage Permission is granted", false)
+                Constants.showImageChooser(this@UserProfileActivity)
             } else {
 
                 Toast.makeText(this, resources.getString(R.string.read_storage_permission_denied), Toast.LENGTH_LONG).show()
             }
+        }
+    }
+
+    public override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (resultCode == Activity.RESULT_OK) {
+            if (requestCode == Constants.PICK_IMAGE_REQUEST_CODE) {
+                if (data != null) {
+                    try {
+                        // The uri of selected image from phone storage.
+                        val selectedImageFileUri = data.data!!
+
+                        iv_user_photo.setImageURI(selectedImageFileUri)
+
+                    } catch (e: IOException) {
+                        e.printStackTrace()
+
+                        Toast.makeText(this@UserProfileActivity, resources.getString(R.string.image_selection_failed), Toast.LENGTH_SHORT).show()
+                    }
+                }
+            }
+        } else if (resultCode == Activity.RESULT_CANCELED) {
+            // A log is printed when user close or cancel the image selection.
+            Log.e("Request Cancelled", "Image selection cancelled")
         }
     }
 }
--- app/src/main/java/com/kingstek/shopit/utils/Constants.kt
+++ app/src/main/java/com/kingstek/shopit/utils/Constants.kt
@@ -1,5 +1,9 @@
 package com.kingstek.shopit.utils
 
+import android.app.Activity
+import android.content.Intent
+import android.provider.MediaStore
+
 object Constants {
 
     const val USERS: String = "users"
@@ -11,6 +15,14 @@ object Constants {
 
     const val READ_STORAGE_PERMISSION_CODE = 2
 
-    const val PICK_IMAGE_REQUEST_CODE = 2
+    const val PICK_IMAGE_REQUEST_CODE = 1
+
+
+    fun showImageChooser(activity: Activity) {
+        // An intent for launching the image selection of phone storage.
+        val galleryIntent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+        // Launches the image selection of phone storage using the constant code.
+        activity.startActivityForResult(galleryIntent, PICK_IMAGE_REQUEST_CODE)
+    }
 
 }
