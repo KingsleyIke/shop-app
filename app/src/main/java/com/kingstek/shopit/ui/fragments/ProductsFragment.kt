2bfa05e935ab8dc71f00cf223b411dd8cfe18468
KingsleyIke <kingsdanike@gmail.com>
2022-01-14 03:39:00 +0100

    add product activity

--- app/src/main/java/com/kingstek/shopit/ui/fragments/ProductsFragment.kt
+++ app/src/main/java/com/kingstek/shopit/ui/fragments/ProductsFragment.kt
@@ -1,12 +1,18 @@
 package com.kingstek.shopit.ui.fragments
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import com.kingstek.shopit.R
+import com.kingstek.shopit.ui.activities.AddProductActivity
+import com.kingstek.shopit.ui.activities.SettingsActivity
 
 
 class ProductsFragment : Fragment() {
@@ -23,5 +29,24 @@ class ProductsFragment : Fragment() {
         val textView: TextView = root.findViewById(R.id.text_home)
         textView.text = "This is home Fragment"
         return root
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.add_product_menu, menu)
+        super.onCreateOptionsMenu(menu, inflater)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+
+        val id = item.itemId
+
+        when (id) {
+            R.id.action_add_product -> {
+                startActivity((Intent(activity, AddProductActivity::class.java)))
+                return true
+            }
+        }
+
+        return super.onOptionsItemSelected(item)
     }
 }
